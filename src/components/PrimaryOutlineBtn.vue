11c40b164037dbd64f8fd322a38dba21cc0f79bd
letrananhvu <vule5726@gmail.com>
2023-12-16 21:11:25 +0200

    Update PrimaryOutlineBtn.vue

--- src/components/PrimaryOutlineBtn.vue
+++ src/components/PrimaryOutlineBtn.vue
@@ -13,6 +13,6 @@
     @apply ring-black ring-2 
     text-base font-semibold text-black w-full py-2 rounded
     hover:bg-black hover:text-white 
-    active:bg-black active:text-white
+    active:bg-black active:text-white;
 }
 </style>
