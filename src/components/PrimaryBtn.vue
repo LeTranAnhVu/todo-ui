31a5eb1b813b95a6272d5bf83a11b21d3d306c21
letrananhvu <vule5726@gmail.com>
2023-12-16 21:11:17 +0200

    Create PrimaryBtn.vue

---
+++ src/components/PrimaryBtn.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <button class="primary-btn">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.primary-btn {
+    @apply text-base font-semibold w-full py-2 rounded
+    text-white bg-black
+    hover:bg-gray-900
+    active:bg-gray-900;
+}
+</style>
