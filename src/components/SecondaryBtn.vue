af5418e34a22541b71490ce7a9f11b7a86ec6d30
letrananhvu <vule5726@gmail.com>
2023-12-16 21:11:20 +0200

    Create SecondaryBtn.vue

---
+++ src/components/SecondaryBtn.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <button class="secondary-btn">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.secondary-btn {
+    @apply text-base w-full py-2
+    text-gray-600 bg-transparent
+    hover:text-black
+    active:text-black;
+}
+</style>
