53cb84b557547dac640aa31ad17c650b7b64b7e0
letrananhvu <vule5726@gmail.com>
2023-12-16 09:45:56 +0200

    Add eslint typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,14 @@ module.exports = {
   env: {
     node: true,
   },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
+    "@vue/typescript/recommended",
     'prettier'
   ],
   rules: {
