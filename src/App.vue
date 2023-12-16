b7b7dfa5b31e201bbd6b6924a7b1e6a11001fe42
letrananhvu <vule5726@gmail.com>
2023-12-16 21:12:22 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 
 import LayoutPage from '@/components/layout/Layout.vue'
+import CreateNewTaskPage from '@/pages/CreateNewTaskPage.vue'
 import TaskPage from '@/pages/TaskPage.vue'
 </script>
 
 <template>
     <div class="max-w-[500px] mx-auto">
         <LayoutPage>
-            <TaskPage/>
+<!--            <TaskPage></TaskPage>-->
+            <CreateNewTaskPage></CreateNewTaskPage>
         </LayoutPage>
     </div>
 </template>
