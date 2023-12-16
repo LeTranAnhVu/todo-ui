11983f6443d3b5ef5f2c363930c6242f95971f76
letrananhvu <vule5726@gmail.com>
2023-12-16 21:11:07 +0200

    Update Checkbox.vue

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -4,53 +4,61 @@ import { ref, toRefs } from 'vue'
 type Props = {
     // checked: boolean
     modelValue: boolean
+    label?: string
 }
 
-const formatBoolean = (value) => value === "true"
+const formatBoolean = (value) => value === 'true'
 const props = defineProps<Props>()
 
-const { modelValue } = toRefs(props)
+const { modelValue, label } = toRefs(props)
 const input = ref(null)
 
 const handleClick = async () => {
     const el = input.value as HTMLInputElement
-    el.value =  !formatBoolean(el.value)
-    el.dispatchEvent(new Event('input')) 
+    console.log('check', el.value)
+    el.value = !formatBoolean(el.value)
+    el.dispatchEvent(new Event('input'))
 }
 </script>
 
 <template>
-    <div class="checkbox" :data-checked="modelValue" @click="handleClick">
-        <input ref="input" :value="modelValue" type="checkbox" @input="$emit('update:modelValue',  formatBoolean($event.target.value))">
+    <div class="checkbox" :data-checked="modelValue">
+        <span v-if="label">{{ label }}</span>
+        <div class="input-wrapper" @click="handleClick">
+            <input ref="input" :value="modelValue" type="checkbox"
+                   @input="$emit('update:modelValue',  formatBoolean($event.target.value))">
+        </div>
     </div>
 </template>
 
 <style scoped>
 .checkbox {
-    @apply relative w-5 h-5;
+    @apply relative
+    flex flex-nowrap justify-between items-center;
 
-    input {
-        @apply appearance-none;
+    .input-wrapper {
+        @apply relative
+        w-5 h-5 rounded-full ring-1 ring-black bg-transparent;
+
+        &:after {
+            @apply block content-[""]
+            w-3.5 h-3.5 rounded-full
+            bg-black
+            absolute top-[50%] -translate-y-1/2 left-[3px]
+            invisible
+            ;
+        }
+
+        input {
+            @apply appearance-none;
+        }
     }
 
-    &:after {
-        @apply block content-[""]
-        w-3.5 h-3.5 rounded-full
-        bg-black
-        absolute top-[50%] -translate-y-1/2 left-[3px]
-        invisible
-        ;
-    }
+    
 
-    &:before {
-        @apply block content-[""]
-        w-5 h-5 rounded-full ring-1 ring-black bg-transparent
-        absolute top-[50%] -translate-y-1/2 left-0
-        ;
-    }
 
     &[data-checked=true] {
-        &:after {
+        & .input-wrapper:after {
             @apply visible;
         }
     }
