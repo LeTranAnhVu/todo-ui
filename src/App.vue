8b15b32ca357b5d097369d7009689c6de559ed3f
letrananhvu <vule5726@gmail.com>
2023-12-16 15:00:26 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 
-import LoginPage from '@/pages/LoginPage.vue'
+import LayoutPage from '@/components/layout/Layout.vue'
+import TaskPage from '@/pages/TaskPage.vue'
 </script>
 
 <template>
     <div class="max-w-[500px] mx-auto">
-        <LoginPage />
+        <LayoutPage>
+            <TaskPage/>
+        </LayoutPage>
     </div>
 </template>
 
