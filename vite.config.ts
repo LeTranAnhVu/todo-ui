828faab035fe2ea325b05abacb0bfb1deb7a7f75
Le Tran Anh Vu <vule5726@gmail.com>
2024-02-18 11:07:37 +0200

    pwa install

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath, URL } from 'url'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL('./src', import.meta.url))
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    plugins: [
+        vue(),
+        VitePWA({
+            manifest: {
+                icons: [
+                    {
+                        src: "/todo_logo.svg",
+                        sizes: 'any',
+                        type: "image/svg+xml",
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        })
+
+    ],
+    server: {
+        port: 3000
     }
-  },
-  plugins: [vue()],
-  server: {
-    port: 3000
-  }
 })
